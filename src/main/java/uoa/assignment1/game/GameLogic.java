000ae0f878389259e77f9442eed32d021f654609
JOSHUA SHAYO <j.shayo.20@abdn.ac.uk>
2021-11-06 20:46:41 +0000

    Task 3 complete

--- src/main/java/uoa/assignment1/game/GameLogic.java
+++ src/main/java/uoa/assignment1/game/GameLogic.java
@@ -9,7 +9,62 @@ import uoa.assignment1.character.Monster;
 public class GameLogic {
 
 	public static void moveCharacter(String input, Map gameMap, GameCharacter character) {
-		
+		switch (input){
+            case "up":
+                moveUp(character, gameMap);
+                break;
+            case "down":
+                moveDown(character, gameMap);
+                break;
+            case "left":
+                moveLeft(character, gameMap);
+                break;
+            case "right":
+                moveRight(character, gameMap);
+                break;
+            default:
+                System.out.println("Use only keywords up, down, left, right");
+        }
 	}
+    
+    private static void moveRight(GameCharacter character, Map gameMap){
+        if(character.column < (gameMap.layout[character.row].length - 1)){
+            String character_icon = gameMap.layout[character.row][character.column];
+            gameMap.layout[character.row][character.column] = ".";
+            character.column += 1;
+            gameMap.layout[character.row][character.column] = character_icon;
+        }
+        
+    }
+    
+    private static void moveLeft(GameCharacter character, Map gameMap){
+        if(character.column > 0){
+            String character_icon = gameMap.layout[character.row][character.column];
+            gameMap.layout[character.row][character.column] = ".";
+            character.column -= 1;
+            gameMap.layout[character.row][character.column] = character_icon;
+        }
+        
+    }
+    
+    private static void moveUp(GameCharacter character, Map gameMap){
+        if(character.row > 0){
+            String character_icon = gameMap.layout[character.row][character.column];
+            gameMap.layout[character.row][character.column] = ".";
+            character.row -= 1;
+            gameMap.layout[character.row][character.column] = character_icon;
+        }
+        
+    }
+    
+    private static void moveDown(GameCharacter character, Map gameMap){
+        if(character.row < gameMap.layout.length - 1){
+            String character_icon = gameMap.layout[character.row][character.column];
+            gameMap.layout[character.row][character.column] = ".";
+            character.row += 1;
+            gameMap.layout[character.row][character.column] = character_icon;
+        }
+        
+    }
 	
 }
--- src/main/java/uoa/assignment1/game/RunGame.java
+++ src/main/java/uoa/assignment1/game/RunGame.java
@@ -1,13 +1,25 @@
 package uoa.assignment1.game;
 
+import java.util.Scanner;
+
 public class RunGame {
 
 	private static boolean gameOver = false;
 
 	public static void main(String[] args) {
-		
+        Game game = new Game(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        int roundCounter = 1;
         while (!gameOver) {
 			//complete the code
+            
+            System.out.println("Round " + roundCounter++);
+            Scanner user_intput = new Scanner(System.in);
+            System.out.println("Player is moving " + user_intput.nextLine());
+            
+            
+            
+            
+            
 		}
 	}
 
